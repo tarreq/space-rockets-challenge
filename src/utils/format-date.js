31517ba4e1c7976463918816d17778d7f895605c
Tareq Hasan <tarreeq@gmail.com>
2021-09-25 19:37:02 +0200

    Fix launch datetime to be displayed in local timezone of launch site

--- src/utils/format-date.js
+++ src/utils/format-date.js
@@ -1,3 +1,5 @@
+import { parseZone } from 'moment'
+
 export function formatDate(timestamp) {
   return new Intl.DateTimeFormat("en-US", {
     weekday: "long",
@@ -7,14 +9,22 @@ export function formatDate(timestamp) {
   }).format(new Date(timestamp));
 }
 
-export function formatDateTime(timestamp) {
-  return new Intl.DateTimeFormat("en-US", {
+export function formatDateTime(timestamp, local = false) {
+  const launchSiteUtcOffset = parseZone(timestamp).utcOffset();
+  const localUserUtcOffset = new Date().getTimezoneOffset();
+
+  // Construct a proper date object which represents local launch date
+  const launchTimeLocal = new Date(new Date(timestamp).getTime() + (launchSiteUtcOffset * 60000) + (localUserUtcOffset * 60000));
+  
+  const formattedTime = new Intl.DateTimeFormat("en-US", {
     year: "numeric",
     month: "long",
     day: "numeric",
     hour: "numeric",
     minute: "numeric",
     second: "numeric",
-    timeZoneName: "short",
-  }).format(new Date(timestamp));
+    hour12: false,
+    ...(!local && {timeZoneName: "short"})
+  }).format(local ? launchTimeLocal : new Date(timestamp));
+  return local ? `${formattedTime} GMT${launchSiteUtcOffset / 60}` : formattedTime
 }
